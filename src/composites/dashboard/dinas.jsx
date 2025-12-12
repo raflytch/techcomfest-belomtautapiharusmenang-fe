de1c3beddde4d8594d7400a477c75ce664e751af
Rafly Aziz Abdillah <raflyazizabdillah30@gmail.com>
2025-12-12 08:42:37 +0700

    chore(dashboard): add useDebounce hook import for filtering functionality

--- src/composites/dashboard/dinas.jsx
+++ src/composites/dashboard/dinas.jsx
@@ -45,6 +45,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import FullscreenLoader from "@/components/ui/fullscreen-loader";
 import { cn } from "@/lib/utils";
+import { useDebounce } from "@/hooks/use-debounce";
 
 const ActionsMap = dynamic(() => import("./actions-map"), {
   ssr: false,
